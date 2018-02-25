c172d0b54c1df28d5032d5d457096d5f6539ca4b
Mor Kenane <morkenan@hotmail.com>
2018-02-25 01:44:13 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,14 +58,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash = 
  		{
@@ -79,8 +79,17 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+ }
 
-    
+programmer_hash
+
+
+
+
 end
 
 def changing_alan
@@ -99,10 +108,11 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+ 
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -122,4 +132,7 @@ def adding_to_dennis
         }
      }
 
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+
+programmer_hash
 end
